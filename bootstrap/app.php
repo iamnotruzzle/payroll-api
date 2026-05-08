10f00a4a21af4ac2cfdd34fcdcbf226e905c4ac8
NOTRuzzle <43885659+iamnotruzzle@users.noreply.github.com>
2026-05-08 11:37:42 +0800

    reset

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,23 +6,13 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        channels: __DIR__.'/../routes/channels.php',
+        channels: __DIR__ . '/../routes/channels.php',
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        // custom middleware aliases
-        $middleware->alias([
-            'role' => \App\Http\Middleware\CheckRole::class,
-        ]);
-
-        // Enable CORS for API
-        $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
-    })
+    ->withMiddleware(function (Middleware $middleware) {})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\Role\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,32 +12,6 @@ Route::middleware(["throttle:{$loginRateLimit},1"])->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
 });
 
-// Broadcasting auth
-Route::post('/broadcasting/auth', function (Request $request) {
-    return Broadcast::auth($request);
-})->middleware('auth:sanctum');
-
-// Protected routes
-$apiRateLimit = env('RATE_LIMIT_API', 60);
-Route::middleware(['auth:sanctum', "throttle:{$apiRateLimit},1"])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
-
-    // super-admin and admin
-    Route::prefix('users')->middleware('role:super-admin|admin')->group(function () {
-        Route::get('/', [UserController::class, 'index']);
-        Route::get('/status-counts', [UserController::class, 'statusCounts']);
-        Route::post('/', [UserController::class, 'store']);
-        Route::post('/{user}/update', [UserController::class, 'update']);
-        Route::delete('/{user}', [UserController::class, 'destroy']);
-    });
-
-    // super-admin
-    Route::prefix('roles')->middleware('role:super-admin|admin')->group(function () {
-        Route::get('/', [RoleController::class, 'index']);
-        Route::get('/{role}', [RoleController::class, 'show']);
-        Route::post('/', [RoleController::class, 'store']);
-        Route::put('/{role}', [RoleController::class, 'update']);
-        Route::delete('/{role}', [RoleController::class, 'destroy']);
-    });
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Route::post('/logout', [AuthController::class, 'logout']);
 });
